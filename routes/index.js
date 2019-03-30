a4c48271f9a6d562b4862bc5744ea7d907ec17d2
Roger Garcia <r1ger_pg@hotmail.com>
2019-03-30 20:00:34 +0530

    Created router file - index.js

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,43 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index');
+const formidable = require('formidable');
+
+const nodemailer = require('../config/nodemailer');
+const transporter = nodemailer.transporter;
+const mailOptions = nodemailer.mailOptions;
+
+router.get('/', function (req, res) {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+router.post('/', function (req, res){
+
+  let form = new formidable.IncomingForm();
+
+  form.parse(req);
+
+  form.on('fileBegin', function (name, file){
+    console.log('__dirname' + __dirname);
+    file.path = '/Users/Roger/file-translator/data/test.txt';
+  });
+
+
+  form.on('file', function (name, file){
+      console.log('Uploaded ' + file.name);
+
+      transporter.sendMail(mailOptions, function(error, info){
+        if (error) {
+          console.log(error);
+        } else {
+          console.log('Email sent: ' + info.response);
+        }
+      });
+      
+  });
+  
+  return res.status(200).json({result: 'Upload Success'})
+
 });
 
 module.exports = router;
