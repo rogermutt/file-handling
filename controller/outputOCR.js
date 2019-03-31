cb738299fdf90eaf39e5f96300f532e9d1d44384
Roger Garcia <r1ger_pg@hotmail.com>
2019-03-31 17:26:11 +0530

    Add OCR functionality

---
+++ controller/outputOCR.js
@@ -0,0 +1,19 @@
+const Tesseract = require('tesseract.js')
+
+async function imageOutput (file) {
+
+    let output = {}
+    
+    await Tesseract
+    .recognize(file.path)
+    .catch(err => console.error(err))
+    .then(result => {
+        output.text = result.text
+        output.confidence = result.confidence
+    })
+
+    return await output   
+
+}
+
+module.exports = imageOutput;  
